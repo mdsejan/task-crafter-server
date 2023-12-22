5bf162b29926057d0bfb06cf1a82a8f3d05f2363
Sejan~ <md4sejan@gmail.com>
2023-12-22 18:35:13 +0600

    project setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// MiddleWare
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Task Crafter server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Task Crafter is running on port: ${port}`);
+})
